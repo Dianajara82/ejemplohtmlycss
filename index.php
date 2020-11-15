1aea9615749aacce0f0f97783f7abbf24f8b5fb8
Diana Arismendi <dianajara82@gmail.com>
2020-11-15 14:29:26 -0500

    se agrega tipografia para el titulo

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Ejemplo2 html y css
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+    <!--Esto es un titulo-->
+    <h1>Biografia de Halsey</h1>
+
+    <img id="halsey1" src="images/Halsey.jpg" width="400" height="250">
+
+</body>
+
+</html>
